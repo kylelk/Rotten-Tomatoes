598a01e259c32f44b7e451cc6f6adc8d9f0a18ae
kyle kersey <kylelk@comcast.net>
2014-05-16 08:29:33 -0700

    Revert "appied suggestions"
    This reverts commit 9a57dcf5e7d9aef77270945e7958abca0aefb5c9.

--- RottenTomatoes.py
+++ RottenTomatoes.py
@@ -67,9 +67,12 @@ class Cache:
             c = conn.cursor()
             insert = """ INSERT OR REPLACE INTO movies
                 (search_query, page_number, timestamp, search_results) VALUES
-                (?, ?, ?, ?); """
+                (\"{search_query}\", {page_number}, {timestamp}, ?); """
             
-            c.execute(insert, (search_query, page_number, timestamp, search_results))
+            insert = insert.format(search_query = search_query,
+                                   page_number = page_number,
+                                   timestamp = timestamp)
+            c.execute(insert, (search_results,))
             conn.commit()
 
 class RottenTomatoes:
